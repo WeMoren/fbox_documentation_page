fd082df63a081b9131afe8e04437999a6549fefb
uwem4life <wemoren@gmail.com>
2025-03-14 16:40:44 +0100

    modified scrn size

--- app.js
+++ app.js
@@ -24,9 +24,11 @@ closeNav.onclick = () => navSection.style.display = 'none';
 
 // Close modal and sidenav on outside click
 window.addEventListener('click', (e) => {
+    if(window.innerWidth <= 420){
     if (!navSection.contains(e.target) && e.target !== openNav) {
         navSection.style.display = 'none'; // Close sidenav
     }
+}
     if (e.target === modal) {
         modal.style.display = 'none'; // Close modal
         body.style.overflow = 'auto'; // Restore scrolling
